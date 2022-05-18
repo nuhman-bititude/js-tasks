08acda149e1471b8f1ee746a245d66bb311a9bef
nuhman-bititude <nuhman@bititude.com>
2022-05-18 17:06:13 +0530

    Task % Completed

---
+++ task-5.js
@@ -0,0 +1,25 @@
+var read = require("readline-sync");
+
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      console.log(num1 + num2);
+      break;
+    case "-":
+      console.log(num1 - num2);
+      break;
+    case "/":
+      console.log(num1 / num2);
+      break;
+    case "*":
+      console.log(num1 * num2);
+      break;
+    default:
+        console.log("Enter a Valid Operator!!!")
+  }
+}
+
+operator = read.question("Specify an Operation  (+,-,/,*)  :")
+num1 = Number (read.question("Enter Number 1: "))
+num2 = Number (read.question("Enter Number 2: "))
+calculator(num1, num2, operator);
