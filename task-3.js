e6ce2e4ad5537a52347778f472b4c2c82d8af5dd
nuhman-bititude <nuhman@bititude.com>
2022-05-18 16:32:43 +0530

    Task 3 Completed,

---
+++ task-3.js
@@ -0,0 +1,14 @@
+// Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar
+var read=require('readline-sync')
+function leapYear(year){
+    // console.log(typeof(year))
+    if ((year%4==0)&&(year%400==0||year%100!=0)){
+        console.log("Leap Year")
+    }
+    else{
+        console.log("Not Leap Year")
+    }
+}
+
+year=read.question("Enter a Year : ")
+leapYear(Number(year))
