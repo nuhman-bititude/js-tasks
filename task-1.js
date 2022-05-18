cc69aad14457a62cac67f0b851effc837bd8e506
nuhman-bititude <nuhman@bititude.com>
2022-05-18 15:54:40 +0530

    Task 1

---
+++ task-1.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7
+function Area(a,b,c){
+    let s=(a+b+c)/2
+    area=Math.sqrt(s*((s-a)*(s-b)*(s-c)))
+    console.log("Area is :",area)
+}
+
+Area(1,1,1)
