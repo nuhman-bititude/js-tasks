a7031bcfb618952dbf24a4fa79db0ccf0365a316
nuhman-bititude <nuhman@bititude.com>
2022-05-19 13:38:51 +0530

    27 pending

---
+++ task-27.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program to check from three given numbers (non negative integers) that two or all of them have the same rightmost digit
+
+let myFunction=(number1,number2,number3)=>{
+
+}
+
+console.log(myFunction(12,10,22))
