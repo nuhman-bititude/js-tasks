1bf3b898fdd630a026db531813ac636ef959b1c4
nuhman-bititude <nuhman@bititude.com>
2022-05-19 16:31:24 +0530

    Task-30.js completed

--- task-30.js
+++ task-30.js
@@ -1,15 +1,19 @@
 // Write a JavaScript program to replace every character in a given string with the character following it in the alphabet.  
 
 let characterChange=(string)=>{
+    // console.log(string)
     newString=""
     length=string.length
     for(let i=0;i<length;i++){
         // console.log(string[i])
         let char=string[i]
-        ans=console.log(char.charCodeAt())
-        
-        
-
+        c=char.charCodeAt()
+        // console.log(c)
+        a=String.fromCharCode(c+1)
+        newString=newString+a
+        // console.log(a)
+        // console.log(fromCharCode(char))
     }
+    return newString
 }
 console.log(characterChange("Bititude"))
