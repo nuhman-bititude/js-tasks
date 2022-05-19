923e39664ca1bdd1e392f3eb5159532892553f08
nuhman-bititude <nuhman@bititude.com>
2022-05-19 14:59:06 +0530

    added task-27.js

--- task-27.js
+++ task-27.js
@@ -1,7 +1,16 @@
 // Write a JavaScript program to check from three given numbers (non negative integers) that two or all of them have the same rightmost digit
 
 let myFunction=(number1,number2,number3)=>{
-
+    if ((number1%10==number2%10)&&(number2%10==number3%10)&&(number1%10==number3%10)){
+        return "Last Digits of all are equal"
+    }
+        else if((number1%10==number2)||(number2%10==number3)||(number1%10==number3%10)){
+            return "Last digits of 2 numbers are equal"
+        }
+        else{
+            return "Last digits are not Equal"
+        }
+    
 }
 
-console.log(myFunction(12,10,22))
+console.log(myFunction(11,15,22))
