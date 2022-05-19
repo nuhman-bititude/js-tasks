6a74b7236691096a61289f7b0de30e494fc344fd
nuhman-bititude <nuhman@bititude.com>
2022-05-19 10:06:14 +0530

    Error correction in task-1.js

--- task-1.js
+++ task-1.js
@@ -1,7 +1,7 @@
 // Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7
 function Area(a, b, c) {
   let s = (a + b + c) / 2;
-  area = Math.sqrt(s * ((s - a) * (s - b) * (s - c)));
+  let area = Math.sqrt(s * ((s - a) * (s - b) * (s - c)));
   console.log("Area is :", area);
 }
 
