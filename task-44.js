50cd58c5e38e946e59dadccc8f6318ca354ded8f
nuhman-bititude <nuhman@bititude.com>
2022-05-22 12:04:52 +0530

    task 44

---
+++ task-44.js
@@ -0,0 +1,1 @@
+// Write a JavaScript code to divide a given array of positive integers into two parts. First element goes to first part, second element goes to second part, and third element goes to first part and so on. Now compute the sum of two parts and store into an array of size two. 
