00515160eeed667fc8d4d0c9f37e5b699a549807
nuhman-bititude <nuhman@bititude.com>
2022-05-24 10:11:16 +0530

    added 45

--- task-45.js
+++ task-45.js
@@ -4,10 +4,28 @@
 let checkArray=(array1,array2)=>{
     var flag=true
     for (let i=0;i<array1.length;i++){
+        for(let j=i;j<array1.length;j++){
+        temp=array2[i]
+        array1[i]=array1[j]
+        array1[j]=temp
+        for(let k=0;k<array1.length;k++){
+            if(array1[k]!=array2[k]){
+                flag=false
+                break
+            }
+        }
+        array1[j]=array1[i]
+        array1=temp
         }
         
     }
-    
+    if(flag){
+        return true
+    }
+    else{
+        return false
+    }
+}
 
 
-console.log(checkArray([12,5,77,9],[12,5,77,9]))
+console.log(checkArray([12,5,77,9],[12,55,77,9]))
